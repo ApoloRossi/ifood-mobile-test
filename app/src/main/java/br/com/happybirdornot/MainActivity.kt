22729c78156015792e2f38e86ac1741f127a6ec6
Apolo Rossi <apolorossi@gmail.com>
2019-03-20 21:41:44 -0300

    git init

---
+++ app/src/main/java/br/com/happybirdornot/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.com.happybirdornot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+}
